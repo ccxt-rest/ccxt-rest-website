b98282e9573f451ffed1765a066bfbd4998bbb3e
Travis (from Travis CI) <franz@see.net.ph>
2020-06-21 17:49:36 +0000

    Deploy ccxt-rest/ccxt-rest-website to github.com/ccxt-rest/ccxt-rest-website.git:gh-pages

---
+++ js/exchange-summary-dashboard.js
@@ -0,0 +1,29 @@
+(function() {
+    function loadExchangeSummaryDashboard() {
+        var lastUpdateDateElem = document.getElementById("lastUpdateDate")
+        var exchangeSummaryElem = document.getElementById("exchangeSummary")
+        if (lastUpdateDateElem && exchangeSummaryElem) {
+            var xhr = new XMLHttpRequest();
+    
+            xhr.onload = function () {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    var imageText = ('<img' + xhr.responseText.split('<img')[1]).split('</div>')[0]
+                    var tableText = ('<table' + xhr.responseText.split('<table')[1]).split('</table>')[0] + '</table>'
+                    lastUpdateDateElem.innerHTML = imageText;
+                    exchangeSummaryElem.innerHTML = tableText;
+                } else {
+                    console.log(`The request failed : ${xhr.status}!`);
+                }
+        
+            };
+        
+            xhr.open('GET', 'https://ccxt-rest.github.io/ccxt-rest/exchanges/');
+            xhr.send();
+        } else {
+            setTimeout(loadExchangeSummaryDashboard, 1000);
+        }
+    }
+
+    setTimeout(loadExchangeSummaryDashboard, 1000);
+
+})();
